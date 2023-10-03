caa94908ecd4fd102e4ff96b1f7c096e388aa212
JoseRearte <gonzalezreare@gmail.com>
2023-10-03 15:17:32 +0000

    auto commit 1       20231003 151731

--- src/workflow-inicial/611_CA_reparar_dataset_e.r
+++ src/workflow-inicial/611_CA_reparar_dataset_e.r
@@ -38,8 +38,10 @@ GrabarOutput <- function() {
 
 CorregirCampoMes <- function(pcampo, pmeses) {
   tbl <- dataset[, list(
-    "v1" = shift(get(pcampo), 3, type = "lag"),
-    "v2" = shift(get(pcampo), 1, type = "lead")
+    "v1" = shift(get(pcampo), 1, type = "lag"),
+    "v2" = shift(get(pcampo), 1, type = "lead"),
+    "v3" = shift(get(pcampo), 2, type = "lag"),
+    "v4" = shift(get(pcampo), 3, type = "lag")
   ),
   by = numero_de_cliente
   ]
