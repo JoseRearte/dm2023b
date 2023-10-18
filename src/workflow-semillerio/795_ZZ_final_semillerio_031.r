58fc5deb173268b6b91b63388d356cda189a17f2
JoseRearte <gonzalezreare@gmail.com>
2023-10-18 23:30:09 +0000

    final

--- src/workflow-semillerio/795_ZZ_final_semillerio_031.r
+++ src/workflow-semillerio/795_ZZ_final_semillerio_031.r
@@ -20,12 +20,12 @@ require("lightgbm")
 
 #Parametros del script
 PARAM  <- list()
-PARAM$experimento  <- "ZZ7950_03"
+PARAM$experimento  <- "ZZ7950_031"
 PARAM$exp_input  <- "HT7510_03"
 
 PARAM$modelos_rank  <- c( 1 )  #Que modelos quiero, segun su posicion en el ranking e la Bayesian Optimizacion, ordenado por ganancia descendente
-PARAM$semillerio  <- 50   # cantidad de semillas a utilizar en el semillerio, podrian ser 50 o 100 para mayor estabilidad
-PARAM$semilla_primos  <- 287629  # se utiliza para generar el vector de  PARAM$semillerio  semillas
+PARAM$semillerio  <- 20   # cantidad de semillas a utilizar en el semillerio, podrian ser 50 o 100 para mayor estabilidad
+PARAM$semilla_primos  <- 436507  # se utiliza para generar el vector de  PARAM$semillerio  semillas
 
 PARAM$kaggle$envios_desde  <-  9500L
 PARAM$kaggle$envios_hasta  <- 12000L
